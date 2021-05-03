b4b1f0aa84bf64fef5b9eb013f2368913c1e15a6
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-03 20:50:42 +0700

    add assets and theme

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Color
+Color blue = Color(0xff0000FF);
+Color white = Color(0xffFFFFFF);
+Color darkblue = Color(0xff050A30);
+Color black = Color(0xff1E1514);
+Color babyblue = Color(0xff7EC8E3);
+
+// Text Style
+TextStyle title =
+    TextStyle(fontSize: 20, color: white, fontWeight: FontWeight.bold);
+TextStyle subtitle =
+    TextStyle(fontSize: 16, color: black, fontWeight: FontWeight.w400);
